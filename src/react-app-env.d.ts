abf9b0e1c3cd7d730173ea1e28e536619aa592e6
ryann254 <keddelyronjoz@gmail.com>
2023-03-28 02:48:57 +0300

    feat: add about section and section wrapper

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -2,3 +2,5 @@ declare module '*.png';
 declare module '*.svg';
 declare module '*.jpeg';
 declare module '*.jpg';
+// We are using an older version of react-tilt that doesn't have typescript support
+declare module 'react-tilt';
