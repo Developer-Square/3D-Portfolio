59129bbed5c5c566c50b3865b8416294aed17a71
ryann254 <keddelyronjoz@gmail.com>
2024-01-21 17:40:49 +0300

    feat: fetch about cms data

--- src/graphql/about.ts
+++ src/graphql/about.ts
@@ -1,18 +1,15 @@
 import { gql } from '@apollo/client';
 
-const ABOUT_QUERY = gql`
-	query {
-		workAndEducations {
-			title
-			subtitle
-			body
-		}
-		eventsAndAchievements {
-			title
-			subtitle
-			body
-		}
-	}
+const ABOUT_INFO_QUERY = gql`
+  query FetchAboutInfo($id: ID!) {
+    aboutInfo(where: { id: $id }) {
+      info
+      mainSkills
+      skillIcons {
+        url
+      }
+    }
+  }
 `;
 
-export default ABOUT_QUERY;
+export default ABOUT_INFO_QUERY;
--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,6 +1,11 @@
 import INTRO_SECTION_QUERY from './intro';
-import ABOUT_QUERY from './about';
+import ABOUT_INFO_QUERY from './about';
 import JOBS_QUERY from './jobs';
 import PORTFOLIO_IMAGES_QUERY from './portfolio';
 
-export { INTRO_SECTION_QUERY, ABOUT_QUERY, JOBS_QUERY, PORTFOLIO_IMAGES_QUERY };
+export {
+  INTRO_SECTION_QUERY,
+  ABOUT_INFO_QUERY,
+  JOBS_QUERY,
+  PORTFOLIO_IMAGES_QUERY,
+};
